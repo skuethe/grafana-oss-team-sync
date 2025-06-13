f0f761136e106771dae6e30b70ac5f790449b9ac
skuethe <56306041+skuethe@users.noreply.github.com>
2025-06-13 11:52:29 +0200

    wip: starting adding tests
    Signed-off-by: skuethe <56306041+skuethe@users.noreply.github.com>

---
+++ internal/helpers/helpers_test.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveFromSlice(t *testing.T) {
+
+	type addTest struct {
+		name          string
+		input         []string
+		remove        string
+		casesensitive bool
+		expected      []string
+	}
+
+	var tests = []addTest{
+		{"two should be removed", []string{"one", "two"}, "two", false, []string{"one"}},
+		{"three does not exist", []string{"one", "two"}, "three", false, []string{"one", "two"}},
+		{"case sensitive Two should not be removed", []string{"one", "two"}, "Two", true, []string{"one", "two"}},
+		{"empty slice", []string{}, "two", false, []string{}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if output := RemoveFromSlice(test.input, test.remove, test.casesensitive); !reflect.DeepEqual(output, test.expected) {
+				t.Errorf("got %q, wanted %q", output, test.expected)
+			}
+		})
+	}
+}
